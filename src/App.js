f38b7eae76c816da93c0a4ca6e92886d82c75d01
Johnny <johnathan.mnn@gmail.com>
2023-07-04 16:40:32 -0500

    Error catch working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
 import { useState} from "react";
 import "./assets/styles.css";
-var sectionStyle;
+
 function App() {
   const [allValues, setAllValues] = useState({
     name: "",
     city: "",
     country: "",
+    park: "",
     make: "",
     model: "",
     arrangement: "",
     status: "",
     link:"" ,
-    main:""
+    main:"",
+    image1: "",
+    image2:"",
   });
 
   async function newCoaster() {
     const response = await fetch('https://rcdb-api.vercel.app/api/coasters/random');
-    // waits until the request completes...
-    const coaster = await response.json();
-    if(coaster.name === 'unknown' && coaster.pictures.length >= 2 == false){
+    const coaster = await response.json().then({
+      
+    })
+    if(coaster.name == 'unknown' && coaster.pictures.length >= 2 == false){
       console.log(false, coaster)
+      newCoaster()
     } else {
-      console.log(true, coaster)
+      try{
+        console.log(true, coaster,  coaster.pictures[0].url)
+      setAllValues(() => ({
+        name: coaster.name,
+        city: coaster.city,
+        country: coaster.country,
+        park: coaster.park.name,
+        make: coaster.make,
+        model: coaster.model,
+        arrangement: coaster.arrangement,
+        status: coaster.status,
+        link: "https://rcdb.com/"+coaster.link,
+        main: coaster.mainPicture.url,
+        image1: coaster.pictures[0].url,
+        image2: coaster.pictures[1].url,
+      }))
+      } catch(err){
+        console.log("error")
+        newCoaster()
+      }
     }
   }
 
-// const newCoaster = (e) => {
-//   const response = fetch('https://rcdb-api.vercel.app/api/coasters/random')
-//   .then(response => response.json())
-//   .then(json => setAllValues(() => ({
-//     name: json.name,
-//     city: "",
-//     country: "",
-//     make: "",
-//     model: "",
-//     arrangement: "",
-//     status: "",
-//     link:"" ,
-//     main: json.mainPicture.url
-// })))
-// console.log(allValues)
-// }
-
-// window.addEventListener("load", (event) => {
-// newCoaster()
-// });
+window.addEventListener("load", (event) => {
+newCoaster()
+});
   return (
     <main className="container" id="vue">
       <h1>{allValues.name}</h1>
